7c4a8464b3b8c7a6321524b220dc5c1abdbe3514
Yash Gupta <gyash21@gmail.com>
2024-02-29 23:58:49 +0530

    update- pattern shift

--- src/Loops/Pattern12.java
+++ src/Loops/Pattern12.java
@@ -29,7 +29,7 @@ public class Pattern12 {
                     System.out.print("* ");
                 }
                 else {
-                    System.out.print("! ");
+                    System.out.print(" ");
                 }
                 j++;
             }
